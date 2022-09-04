a6e482faa6947da3b1bb4c1c2b6deb6e150996d7
Tushar Roy <binarydevelop@gmail.com>
2022-09-04 17:20:45 +0530

    add: search in a nearly sorted array

---
+++ Binary Search/Problems/5. Search in a nearly sorted array/using binary search.js
@@ -0,0 +1,34 @@
+/* https://www.callicoder.com/binary-search-nearly-almost-sorted-array/ */
+
+
+function searchNearlySortedArray(arr, key ){
+    let start = 0;
+    let end = arr.length - 1;
+    
+    if(arr.length == 1 && arr[0] == key){ 
+        return [0,arr[0]]
+    }
+    
+    while(start < end ){ 
+      let mid = Math.floor(start + (end - start)/2);
+  
+      if(arr[mid] == key ) {
+        return [mid, arr[mid]];
+      }
+      if(arr[mid - 1] == key ) {
+        return [mid - 1, arr[mid - 1]];
+      }
+      if( arr[mid+1] == key ) {
+        return [mid + 1, arr[mid + 1]];
+      }
+      if(arr[mid] < key){
+       start = mid + 2;
+      }
+      if(arr[mid] > key){
+        end = mid - 2;
+      }
+    }
+    return false
+  }
+  
+  console.log(searchNearlySortedArray([10, 3, 40, 20, 50, 80, 70], 20));
