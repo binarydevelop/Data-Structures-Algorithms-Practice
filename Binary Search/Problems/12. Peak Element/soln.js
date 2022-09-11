7a1b282a02e93c789ebbf6f1f9c6ea0b046c05f3
Tushar Roy <binarydevelop@gmail.com>
2022-09-11 18:15:15 +0530

    add: peak element

---
+++ Binary Search/Problems/12. Peak Element/soln.js
@@ -0,0 +1,27 @@
+function solution(arr){
+    let start = 0;
+    let end = arr.length -1 ;
+    
+    if(arr[0] > arr[1]){
+      return arr[0];
+    }
+    if(arr[end] > arr[end -1]){
+      return arr[end];
+    }
+    while(start <= end){
+      let mid = Math.floor(start + (end - start) /2);
+  
+      if(arr[mid] > arr[mid + 1] && arr[mid] > arr[mid -1]){
+        return arr[mid]
+      }
+      if(arr[mid] < arr[mid + 1] ){
+        start = mid + 1;
+      }else {
+        end = mid -1;
+      }
+        
+    }
+  }
+  
+  let arr =[1, 2, 3]
+  console.log(solution(arr));
--- Binary Search/Theory/binarysearch.md
+++ Binary Search/Theory/binarysearch.md
@@ -3,3 +3,4 @@
 If the problem is sorted and the question is to find or search something we should check binary search as an option. 
 
 - [Variants of binary search](https://www.geeksforgeeks.org/variants-of-binary-search/)
+- Please note that if we change the binary search boundary conditions to left <= right, then we need to change to right = mid – 1 accordingly.
