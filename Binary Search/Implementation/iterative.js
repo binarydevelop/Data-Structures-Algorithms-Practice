c1f4ce8367e2b6a0465ab0f873ba6a23304c7b02
Tushar Roy <binarydevelop@gmail.com>
2022-09-19 09:10:13 +0530

    modify iterative binarysearch

--- Binary Search/Implementation/iterative.js
+++ Binary Search/Implementation/iterative.js
@@ -1,17 +1,23 @@
-function iterativeBinarySearch(arr, start, end, key){
+function iterativeBinarySearch(arr, key){
+    let start = 0;
+    let end = arr.length-1;
+    
     while(start <= end){
-        let mid = Math.floor((start + end)/2);
-        if(arr[mid] == key){
-            return mid;
-        }
-        else if(arr[mid] < key){
-            start = mid + 1;
-        } else if(arr[mid] > key){
-            end = mid -1;
-        }
+      let mid = Math.floor(start + (end - start)/ 2);
+      if(arr[mid] == key){ 
+        return mid;
+      }
+      else if(arr[mid] >  key){ 
+        end = mid - 1;
+      } 
+      else if(arr[mid] < key) { 
+        start = mid + 1
+      }
     }
-    return false
-}
-
-let arr = [1, 3, 5, 7, 8, 9];
-console.log(iterativeBinarySearch(arr, 0, arr.length-1, 5));
+    return -1
+  }
+  
+  let arr = [1,3,6,7,21,22,67];
+  let key = 22
+  
+  console.log(iterativeBinarySearch(arr, key));
