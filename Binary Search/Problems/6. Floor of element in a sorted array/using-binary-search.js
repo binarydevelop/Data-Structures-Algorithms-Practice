cf0f80ba992392e6f81170245e18543266038f04
Tushar Roy <binarydevelop@gmail.com>
2022-10-13 17:11:54 +0530

    fix: Floor of element

--- Binary Search/Problems/6. Floor of element in a sorted array/using-binary-search.js
+++ Binary Search/Problems/6. Floor of element in a sorted array/using-binary-search.js
@@ -1,28 +1,24 @@
-function floorOfElem(arr, key){
-    let start = 0;
-    let end = arr.length - 1;
-    let result = -1;
-    if(arr[arr.length -1 ] < key){
-      return arr[arr.length -1 ]
-    }
-    if(arr[0] > key){
-      return -1
-    }
-    while(start <= end){
-      let mid = Math.floor(start + (end - start)/2)
-      if(arr[mid] == key){
-        result = arr[mid];
-        return result
+
+
+    
+function findFloor(arr, n, x)
+  {
+      if(x < arr[0]){
+          return -1;
       }
-      if(arr[mid] < key && arr[mid] > result){
+      
+      let start = 0 ;
+      let end = arr.length -1;
+      
+      while(start <= end){
+          let mid = Math.floor(start + (end - start)/2);
+          if(arr[mid] > x){
+              end = mid - 1;
+          } else {
               start = mid + 1;
-              result = arr[mid];
+          }
       }
-      if(arr[mid] > key){
-        end = mid - 1;
-      }
-    }
-  return result;
+    return end ; 
   }
   
   let arr = [1,2,3,8,10,12,19];
