4255fbb4d29e859127131e7d241b76a6b674fbae
Tushar Roy <binarydevelop@gmail.com>
2022-10-18 17:21:20 +0530

    add: reverse String

---
+++ Two -pointer Technique/problems/06. Reverse String/soln.js
@@ -0,0 +1,18 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ function reverseString(arr){
+    let start =0;
+   let end = arr.length -1;
+   
+   while(start <= end){
+     let temp = arr[start];
+     arr[start] = arr[end];
+     arr[end] = temp;
+     start++;
+     end--;
+   }
+   return arr;
+ }
+ 
