4477de18ebbcf2dace02c6faa586d487c7f9ece4
Tushar Roy <binarydevelop@gmail.com>
2022-10-22 16:32:39 +0530

    updated two sum solution

--- Two -pointer Technique/problems/01. Pair Sum/soln.js
+++ Two -pointer Technique/problems/01. Pair Sum/soln.js
@@ -1,20 +1,15 @@
-function pairSum(arr, target){
-    let ptr1 = 0;
-    let ptr2 = arr.length - 1; 
-    let results = [];
-    while(ptr1 < ptr2){
-            if(arr[ptr1] + arr[ptr2] == target){
-                results.push( [ptr1, ptr2])
-                ptr1++;
-            }
-            if(arr[ptr1] + arr[ptr2] < target){
-                ptr1++;
-            }else {
-                ptr2--;
-            }
+var twoSum = function(nums, target) {
+    let map = new Map();    
+    for(let i =0; i< nums.length; i++ ){
+        let compliment = target - nums[i];
+    
+        if(map.has(compliment)){
+            return [map.get(compliment), i]
+        } else {
+            map.set(nums[i], i)
+        }    
     }
-            return results
-  }
-  console.log(pairSum([1,2,3,4,5],9))
-
-  /* https://leetcode.com/problems/two-sum/ */
+    return []
+    };
+    
+/* https://leetcode.com/problems/two-sum/ */
