cf9583db093bc4a446f252ee3585292a6c9b0ca0
Tushar Roy <binarydevelop@gmail.com>
2023-04-27 05:44:42 +0530

    adds stable method for finding no of digits

--- Mathematics/01. Find No of Digits/01. Find no of Digits.js
+++ Mathematics/01. Find No of Digits/01. Find no of Digits.js
@@ -9,3 +9,17 @@ const findNoOfDigits= (n) => {
 
 // taking n>=1 so that when input comes to 1 then 1/10 = 0.1 and
 //  greater than 0 everytime and thus the program will go in an infinite loop
+
+// Better and safe solution below
+// Taking Floor means 0.1 == 0
+
+function soln(n){
+    let count = 0;
+    while(n>0){
+      count++;
+      n = Math.floor(n/10);
+    }
+    console.log(count)
+  }
+  
+  soln(2);
