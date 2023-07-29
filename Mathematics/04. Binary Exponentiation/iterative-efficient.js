e5c938d67a5af79b74933a5c53f64c0358a71fc5
binarydevelop <binarydevelop@gmail.com>
2023-07-29 16:11:42 +0530

    adds iterative power

---
+++ Mathematics/04. Binary Exponentiation/iterative-efficient.js
@@ -0,0 +1,14 @@
+function computePower(x,n){
+    let res = 1;
+    while(n>0){
+        if(n%2!=0){
+            res = res * x;
+        }
+
+        x= x*x;
+        n=Math.floor(n/2);
+    }
+    return res;
+}
+
+console.log(computePower(2,3));
