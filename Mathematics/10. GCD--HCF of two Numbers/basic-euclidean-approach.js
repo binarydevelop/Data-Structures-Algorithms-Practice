eeb212d86da58d011ea9155458dfd859d3ffb9d0
Tushar Roy <binarydevelop@gmail.com>
2023-09-01 17:24:47 +0530

    fix code

--- Mathematics/10. GCD--HCF of two Numbers/basic-euclidean-approach.js
+++ Mathematics/10. GCD--HCF of two Numbers/basic-euclidean-approach.js
@@ -8,3 +8,13 @@ function gcd(a,b){
     }
     return a;
 }
+
+/* 
+Process of Elimination:
+
+This loop essentially performs a process of elimination by subtracting the smaller of the two numbers from the larger one repeatedly
+until they become equal.
+The reason behind this process is that the GCD of two numbers is the largest number that divides both of them.
+By subtracting the smaller number from the larger one,
+we are effectively reducing the problem to finding the GCD of the difference and the smaller number.
+*/
