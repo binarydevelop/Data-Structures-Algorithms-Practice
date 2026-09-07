1068ad73137ffd63e1af0ad87f3caf4ff6214a8f
Tushar Roy <61935187+binarydevelop@users.noreply.github.com>
2026-09-07 20:43:06 +0530

    Implement bubble sort algorithm
    Added bubble sort function for sorting an array.

--- 00. Algorithms/bubble_sort.js
+++ 00. Algorithms/bubble_sort.js
@@ -14,3 +14,21 @@ function bubbleSort(arr) {
 }
 
 console.log(bubbleSort([5, 3, 8, 4, 2]));
+
+
+// Optimised 
+function selectionSort(nums) {
+    for(let i =0; i < nums.length; i++){
+        let flag = false; // because if the flag is not swapped that means the array is already sorted
+        for(let j = 0; j < nums.length- 1- i; j++){ // nums.length-1- i is because we subtract one because we compare j with j+1 and i because the last i elements will already be sorted after ith pass. 
+            if(nums[j] > nums[j+1]){
+                [nums[j], nums[j+1]] = [nums[j+1], nums[j]]
+                flag = true // this flips the flag so we know swapping happend 
+            }
+        }
+        if(flag === false) return nums
+    }
+    return nums
+}
+
+console.log(selectionSort([2,1,4,5,3,7]));
